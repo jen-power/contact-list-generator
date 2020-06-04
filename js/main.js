7efaf81284bc4cd075cd2d5bbbb327dfe17b892e
Jen Power <powe0309@algonquinlive.com>
2020-06-04 12:07:43 -0400

    Add closing tag to dl

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var listContacts = function (contacts) {
 
   contacts.forEach(function (item) {
     document.write(`<h2>${item.name}</h2>`);
-    document.write('<dl');
+    document.write('<dl>');
     document.write('<dt>E-mail address</dt>');
     document.write(`<dd><a href="mailto:${item.email}">${item.email}</a></dd>`);
     document.write('<dt>Phone number</dt>');
