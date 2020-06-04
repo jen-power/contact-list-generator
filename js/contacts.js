8fd8effdf2c1f5b50618983564168a5933a7b88c
Jen Power <powe0309@algonquinlive.com>
2020-06-04 11:51:53 -0400

    Output variables in main

--- js/contacts.js
+++ js/contacts.js
@@ -2,7 +2,7 @@ var peeps = [
   {
     name: 'Mercury Man',
     email: 'theman@mercury.com',
-    tel: '05551234',
+    tel: '+05551234',
     loc: [46001200, 69816900]
   },
   {
@@ -10,11 +10,17 @@ var peeps = [
     email: 'venus@theadventurer.com',
     tel: '+15551234',
     loc: [107477000, 108939000]
-  };
+  },
   {
     name: 'Jupiter Juggernaut',
     email: 'thejugger@jupiter.com',
     tel: '+55551234',
     loc: [816040000, 740550000]
+  },
+  {
+    name: 'Saturn',
+    email: 'thesaturn@ring.com',
+    tel: '+927927910',
+    loc: [2983793,9823920002]
   }
 ];
